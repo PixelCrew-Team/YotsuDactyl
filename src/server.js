239d99d4f3cf1944f772f26d70724d8fcff63e51
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-12 16:54:54 -0400

    Crear src/server.js

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.locals.siteName = config.siteName;
+    res.locals.siteUrl = config.siteUrl;
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.get('/dash', (req, res) => {
+    res.render('dashboard');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`${config.siteName} running on port ${PORT}`);
+});
