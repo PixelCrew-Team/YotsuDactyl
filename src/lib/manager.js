9dfefcaa47e2581bb7ed0123d9326d6a3746e878
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-13 09:43:14 -0400

    Actualizar manager.js

--- src/lib/manager.js
+++ src/lib/manager.js
@@ -4,13 +4,13 @@ const { createServerContainer } = require('./daemon');
 
 const deployServer = async (server) => {
     const serverPath = path.join('/var/lib/yotsudactyl/volumes', server.identifier);
-    
+
     await fs.ensureDir(serverPath);
-    
+
     await fs.writeFile(path.join(serverPath, 'yotsu.txt'), `Servidor ${server.name} creado correctamente.`);
-    
+
     await createServerContainer(server);
-    
+
     return true;
 };
 
