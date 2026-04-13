a79d45b5aeb567d303538c897bc8734e31a3547b
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-12 22:41:23 -0400

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# YOTSUDACTYL - INSTALLER PRO
 
 AZUL_B="\e[1;34m"
 CYAN_B="\e[1;36m"
@@ -9,53 +10,34 @@ RESET="\e[0m"
 clear
 echo -e "${AZUL_B}########################################${RESET}"
 echo -e "${CYAN_B}      YOTSUDACTYL - INSTALLER          ${RESET}"
-echo -e "${AZUL_B}          Kurayami Style               ${RESET}"
 echo -e "${AZUL_B}########################################${RESET}"
-echo ""
 
-echo -e "${CYAN_B} [1]${RESET} Iniciar instalación completa"
-echo -e "${CYAN_B} [2]${RESET} Configurar base de datos"
-echo -e "${CYAN_B} [3]${RESET} Salir"
-echo ""
-read -p " Selecciona una opción: " opt
+echo -e "\n${AZUL_B}[>]${RESET} Instalando dependencias del sistema y Docker..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y curl ca-certificates gnupg lsb-release
 
-case $opt in
-  1)
-    echo -e "\n${AZUL_B}[>]${RESET} Actualizando sistema..."
-    sudo apt update && sudo apt upgrade -y
-    
-    echo -e "\n${AZUL_B}[>]${RESET} ¿Instalar Node.js y PostgreSQL? (s/n)"
-    read install_deps
-    if [ "$install_deps" = "s" ]; then
-        curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-        sudo apt install -y nodejs postgresql postgresql-contrib
-        echo -e "${VERDE}[!] Dependencias instaladas.${RESET}"
-    fi
+# Instalación de Docker (Vital para los contenedores)
+if ! [ -x "$(command -v docker)" ]; then
+    echo -e "${AZUL_B}[>]${RESET} Instalando Docker Engine..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    echo -e "${VERDE}[!] Docker instalado correctamente.${RESET}"
+else
+    echo -e "${VERDE}[!] Docker ya está instalado.${RESET}"
+fi
 
-    echo -e "\n${AZUL_B}[>]${RESET} Configurando base de datos..."
-    read -p " Nombre para la DB [yotsudactyl]: " db_name
-    db_name=${db_name:-yotsudactyl}
-    
-    sudo -u postgres psql -c "CREATE DATABASE $db_name;"
-    sudo -u postgres psql -d $db_name -f database.sql
-    echo -e "${VERDE}[!] Tablas creadas correctamente.${RESET}"
+# Instalación de Node y Postgres
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs postgresql postgresql-contrib
 
-    echo -e "\n${AZUL_B}[>]${RESET} Instalando paquetes de Node..."
-    npm install
-    
-    echo -e "\n${VERDE}########################################${RESET}"
-    echo -e "${VERDE}   INSTALACIÓN COMPLETADA CON ÉXITO    ${RESET}"
-    echo -e "${VERDE}########################################${RESET}"
-    ;;
-  2)
-    echo -e "${AZUL_B}[!] Reconfigurando base de datos...${RESET}"
-    # Aquí iría lógica solo para DB
-    ;;
-  3)
-    echo "Saliendo..."
-    exit 0
-    ;;
-  *)
-    echo -e "${ROJO}Opción no válida.${RESET}"
-    ;;
-esac
+echo -e "\n${AZUL_B}[>]${RESET} Configurando Base de Datos..."
+sudo -u postgres psql -c "CREATE DATABASE yotsudactyl;"
+sudo -u postgres psql -d yotsudactyl -f database.sql
+
+echo -e "\n${AZUL_B}[>]${RESET} Instalando paquetes de Node.js..."
+npm install
+
+echo -e "\n${VERDE}########################################${RESET}"
+echo -e "${VERDE}   SISTEMA LISTO PARA ARRANCAR         ${RESET}"
+echo -e "${VERDE}########################################${RESET}"
