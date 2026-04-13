33fbfd30604485208a947b424744206a4b766b83
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-12 23:47:42 -0400

    Crear manager.js

---
+++ src/lib/manager.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { createServerContainer } = require('./daemon');
+
+const deployServer = async (server) => {
+    const serverPath = path.join('/var/lib/yotsudactyl/volumes', server.identifier);
+    
+    await fs.ensureDir(serverPath);
+    
+    await fs.writeFile(path.join(serverPath, 'yotsu.txt'), `Servidor ${server.name} creado correctamente.`);
+    
+    await createServerContainer(server);
+    
+    return true;
+};
+
+module.exports = { deployServer };
