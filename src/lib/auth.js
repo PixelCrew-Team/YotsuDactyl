5961c0e8ad29849c04113020950a7851f478a9fd
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-13 09:03:26 -0400

    Actualizar auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,10 +2,14 @@ const db = require('../database');
 
 module.exports = async function(req, res, next) {
     if (req.session && req.session.userId) {
-        const result = await db.query('SELECT * FROM users WHERE id = $1', [req.session.userId]);
-        if (result.rows.length > 0) {
-            res.locals.user = result.rows[0];
-            return next();
+        try {
+            const result = await db.query('SELECT * FROM users WHERE id = $1', [req.session.userId]);
+            if (result.rows.length > 0) {
+                res.locals.user = result.rows[0];
+                return next();
+            }
+        } catch (err) {
+            return res.redirect('/');
         }
     }
     res.redirect('/');
