7cd9cfd8b22520c772a8ab54fb66f7dafa40b8df
PixelCrew Team <pixelcrewteam@gmail.com>
2026-04-13 09:42:44 -0400

    Actualizar database.js

--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'yotsudactyl',
+    password: String(process.env.DB_PASSWORD || 'yotsu_pass'),
     port: process.env.DB_PORT || 5432,
 });
 
